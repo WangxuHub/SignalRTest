9303d8796b4d1180d660e63a502282a461b5870e
chenshuangyu <695166116@qq.com>
2017-05-30 23:27:10 +0800

    添加项目文件。

---
+++ SignalRTest/MyStartup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRTest.MyStartup))]
+
+namespace SignalRTest
+{
+    public class MyStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+            //注册管道,使用默认的虚拟地址,根目录下的"/signalr",当然你也可以自己定义
+            app.MapSignalR();
+        }
+    }
+}
